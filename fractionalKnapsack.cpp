e4b27f4beb9cc8534fda47557d1a3d80cba72d4b
c-cardie <ccardie001@student.franciscan.edu>
2024-07-24 18:55:50 -0400

    Item object successfully created

--- fractionalKnapsack.cpp
+++ fractionalKnapsack.cpp
@@ -1,1 +1,23 @@
-//testing git commit
+#include <iostream>
+using namespace std;
+
+struct Item {
+
+    int weight;
+    int value;
+    double wv_ratio;
+
+    //constructor
+    Item (int w, int v)
+        : weight(w), value(v), wv_ratio (static_cast<double>(w)/v) 
+        {
+            cout << "weight: " << weight << endl;
+            cout << "value: " << value << endl;
+            cout << "weight-value ratio: " << wv_ratio << endl;
+        }
+};
+
+int main() {
+
+    Item gold(5, 20);
+}
